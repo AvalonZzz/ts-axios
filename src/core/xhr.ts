c91a0d15765aef53119763c792a38be1c43f31dd
Avalon <1239115634@qq.com>
2022-03-14 22:38:18 +0800

    修改ts-axios

--- src/core/xhr.ts
+++ src/core/xhr.ts
@@ -1,5 +1,6 @@
-import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from "./types";
-import {parseHeaders} from './helpers/header'
+import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from "../types";
+import { createError } from "../helpers/error";
+import {parseHeaders} from '../helpers/header'
 
 export default function xhr(config: AxiosRequestConfig): AxiosPromise{
   return new Promise((resolve, reject) => {
@@ -15,7 +16,7 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise{
       request.timeout = timeout
     }
   
-    request.open(method.toUpperCase(), url, true)
+    request.open(method.toUpperCase(), url!, true)
 
     request.onreadystatechange = function handleLoad(){
       if (request.readyState !== 4){
@@ -35,11 +36,11 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise{
     }
 
     request.onerror = function handleError() {
-      reject(new Error('Network Error'))
+      reject(createError('Network Error', config, null, request))
     }
 
     request.ontimeout = function handleTimeout(){
-      reject(new Error(`Timeout of ${timeout} ms exceeded`))
+      reject(createError(`Timeout of ${timeout} ms exceeded`, config, 'ECONNABORTED', request))
     }
   
     Object.keys(headers).forEach(name => {
@@ -56,7 +57,7 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise{
       if (response.status >= 200 && response.status < 300){
         resolve(response)
       } else {
-        reject(new Error(`Request failed width status code ${response.status}`))
+        reject(createError(`Request failed width status code ${response.status}`, config, null, request, response))
       }
     }
   })
--- src/types/index.ts
+++ src/types/index.ts
@@ -7,7 +7,7 @@ export type Method = 'get' | 'GET'
   | 'patch' | 'PATCH'
 
 export interface AxiosRequestConfig{
-  url: string
+  url?: string
   method?:Method
   data?: any
   params?: any
@@ -27,4 +27,34 @@ export interface AxiosResponse{
 
 export interface AxiosPromise extends Promise<AxiosResponse>{
 
+}
+
+export interface AxiosError extends Error{
+  config: AxiosRequestConfig
+  code?: string | null
+  request?: any
+  response?: AxiosResponse
+  isAxiosError: boolean
+}
+
+export interface Axios{
+  request(config: AxiosRequestConfig): AxiosPromise
+
+  get(url: string, config?: AxiosRequestConfig): AxiosPromise
+
+  delete(url: string, config?: AxiosRequestConfig): AxiosPromise
+
+  head(url: string, config?: AxiosRequestConfig): AxiosPromise
+
+  options(url: string, config?: AxiosRequestConfig): AxiosPromise
+
+  post(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise
+  
+  put(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise
+  
+  patch(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise
+}
+
+export interface AxiosInstance{
+  (config: AxiosRequestConfig): AxiosPromise
 }
